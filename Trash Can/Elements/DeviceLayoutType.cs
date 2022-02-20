f8ac9cbccda37c2b67f7957954f7d7150295454f
ysdy44 <ysdy44@outlook.com>
2022-02-21 02:38:41 +0800

    Add DeviceLayout

---
+++ Trash Can/Elements/DeviceLayoutType.cs
@@ -0,0 +1,30 @@
+﻿namespace Trash_Can.Elements
+{
+    /// <summary>
+    /// Device layout type.
+    /// </summary>
+    public enum DeviceLayoutType
+    {
+        /// <summary> Person computer. </summary>
+        PC,
+        /// <summary> Pad. </summary>
+        Pad,
+        /// <summary> Phone. </summary>
+        Phone,
+    }
+
+    /// <summary>
+    /// Phone layout type.
+    /// </summary>
+    public enum PhoneLayoutType
+    {
+        /// <summary> Hide left and right borders. </summary>
+        Hided,
+
+        /// <summary> Show left border. </summary>
+        ShowLeft,
+
+        /// <summary> Show right border. </summary>
+        ShowRight,
+    }
+}
