73ba5f8cad20e57aa6b839da9fdb8468b276f257
ysdy44 <ysdy44@outlook.com>
2022-02-21 02:44:29 +0800

    Add Trash

---
+++ Trash Can/Trashs/Trash.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Trash_Can.Trashs
+{
+    public sealed class Trash
+    {
+
+        public string Title { get; set; }
+        public string Comment { get; set; }
+        public IList<string> Keywords { get; set; }
+
+        public Trash Clone()
+        {
+            return new Trash
+            {
+                Title = this.Title,
+                Comment = this.Comment,
+                Keywords = this.Keywords.ToList(),
+            };
+        }
+
+    }
+}
---
+++ Trash Can/Trashs/TrashItem.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace Trash_Can.Trashs
+{
+    public sealed class TrashItem : INotifyPropertyChanged
+    {
+        public string Name { get; set; }
+        public DateTimeOffset DateCreated { get; set; }
+
+        public bool IsFavorite
+        {
+            get => this.isFavorite;
+            set
+            {
+                this.isFavorite = value;
+                this.OnPropertyChanged(nameof(IsFavorite)); // Notify 
+            }
+        }
+        private bool isFavorite;
+
+
+        public Trash Properties
+        {
+            get => this.properties;
+            set
+            {
+                this.properties = value;
+                this.OnPropertyChanged(nameof(Properties)); // Notify 
+                this.OnPropertyChanged(nameof(Title)); // Notify 
+                this.OnPropertyChanged(nameof(Comment)); // Notify 
+                this.OnPropertyChanged(nameof(Keywords)); // Notify 
+            }
+        }
+        private Trash properties;
+
+        public string Title => this.Properties.Title;
+        public string Comment => this.Properties.Comment;
+        public IList<string> Keywords => this.Properties.Keywords;
+
+
+
+        //@Notify 
+        /// <summary> Multicast event for property change notifications. </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+        /// <summary>
+        /// Notifies listeners that a property value has changed.
+        /// </summary>
+        /// <param name="propertyName"> Name of the property used to notify listeners. </param>
+        protected void OnPropertyChanged(string propertyName) => this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
