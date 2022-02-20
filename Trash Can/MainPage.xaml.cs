c902e916c469f2904b3f9f7bc97d41fecfea6709
ysdy44 <ysdy44@outlook.com>
2022-02-21 02:29:32 +0800

    添加项目文件。

---
+++ Trash Can/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace Trash_Can
+{
+    public sealed partial class MainPage : Page
+    {
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+    }
+}
