3f3879137a68930034e34306bbbe407f715a87ef
ysdy44 <ysdy44@outlook.com>
2022-02-21 02:42:58 +0800

    Add AccentDecoratorItem

---
+++ Trash Can/Elements/AccentDecoratorItem.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Trash_Can.Elements
+{
+    /// <summary>
+    /// Item of <see cref="AccentDecorator"/>.
+    /// </summary>
+    public sealed partial class AccentDecoratorItem : UserControl
+    {
+
+        //@Converter
+        private Visibility BooleanToVisibilityConverter(bool value) => value ? Visibility.Visible : Visibility.Collapsed;
+
+
+        #region DependencyProperty
+
+
+        /// <summary> Gets or sets the IsFavorite of<see cref = "AccentDecoratorItem" />. </summary>
+        public bool IsFavorite
+        {
+            get => (bool)base.GetValue(IsFavoriteProperty);
+            set => base.SetValue(IsFavoriteProperty, value);
+        }
+        /// <summary> Identifies the <see cref = "AccentDecoratorItem.IsFavorite" /> dependency property. </summary>
+        public static readonly DependencyProperty IsFavoriteProperty = DependencyProperty.Register(nameof(IsFavorite), typeof(bool), typeof(AccentDecoratorItem), new PropertyMetadata(false));
+
+
+        #endregion
+
+
+        //@Construct
+        /// <summary>
+        /// Initializes a AccentDecoratorItem. 
+        /// </summary> 
+        public AccentDecoratorItem()
+        {
+            this.InitializeComponent();
+        }
+
+    }
+}
