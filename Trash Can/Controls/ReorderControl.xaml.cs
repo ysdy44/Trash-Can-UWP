971bc3f9d345d88cb38ad64bc5126373e14db02a
ysdy44 <ysdy44@outlook.com>
2022-02-21 02:51:51 +0800

    Add ReorderControl

---
+++ Trash Can/Controls/ReorderControl.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Trash_Can.Elements;
+using Windows.UI.Xaml.Input;
+
+namespace Trash_Can.Controls
+{
+    public sealed partial class ReorderControl : Reorder
+    {
+        public string Text
+        {
+            get => this.TextBox.Text;
+            set => this.TextBox.Text = value;
+        }
+
+        public ReorderControl()
+        {
+            this.InitializeComponent();
+            this.Thumb.ManipulationMode = ManipulationModes.TranslateY;
+            this.Thumb.ManipulationStarted += base.ThumbManipulationStarted;
+            this.Thumb.ManipulationDelta += base.ThumbManipulationDelta;
+            this.Thumb.ManipulationCompleted += base.ThumbManipulationCompleted;
+        }
+    }
+}
